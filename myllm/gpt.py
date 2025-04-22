278299a6053c5f421e96793da595beafd91ab595
Victor Hugo Germano <victorhg@gmail.com>
2025-04-22 16:39:09 -0700

    moved generate to GPTModel

--- myllm/gpt.py
+++ myllm/gpt.py
@@ -81,3 +81,30 @@ class GPTModel(nn.Module):
 
         logits = self.out_head(x)
         return logits
+
+    def generate(self, idx, max_new_tokens, context_size,
+             temperature=0.0, top_k=None, eos_id=None):
+
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -context_size:]
+            with torch.no_grad():
+                logits = self(idx_cond)
+            logits = logits[:, -1, :]
+            if top_k is not None:  
+                top_logits, _ = torch.topk(logits, top_k)
+                min_val = top_logits[:,  -1]
+                logits = torch.where( logits < min_val,
+                    torch.tensor(float('-inf')).to(logits.device),
+                    logits
+                )
+            if temperature > 0.0: 
+                logits = logits / temperature
+                probs = torch.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+            
+            if idx_next == eos_id: break
+
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
