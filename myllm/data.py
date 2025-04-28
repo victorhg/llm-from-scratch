bc91bf2d57dac9519648a280cfb5389437578a0f
Victor Hugo Germano <victorhg@gmail.com>
2025-04-28 14:52:34 -0700

    Added classifier training for fine tuned model

--- myllm/data.py
+++ myllm/data.py
@@ -1,5 +1,6 @@
 import torch
 import tiktoken
+import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
 class GPTDatasetV1(Dataset):
@@ -20,6 +21,48 @@ class GPTDatasetV1(Dataset):
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
 
+# Padding
+class SpamDataset(Dataset):
+    PAD_TOKEN_ID = 50256  # <|endoftext|> token from gpt2
+    def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=PAD_TOKEN_ID):
+        self.data = pd.read_csv(csv_file)
+        self.encoded_texts = [ tokenizer.encode(text) for text in self.data["Text"] ]
+        
+        if max_length is None:
+            self.max_length = self._longest_encoded_length()
+        else:
+            self.max_length = max_length
+            self.encoded_texts = [
+                encoded_text[:self.max_length] 
+                for encoded_text in self.encoded_texts
+            ]
+        
+        # pad all batches
+        self.encoded_texts = [
+            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
+            for encoded_text in self.encoded_texts
+        ]
+
+    def __getitem__(self, index):
+        encoded = self.encoded_texts[index]
+        label = self.data.iloc[index]["Label"]
+        return (
+            torch.tensor(encoded, dtype=torch.long),
+            torch.tensor(label, dtype=torch.long)
+        )
+
+    def __len__(self):
+        return len(self.data)
+    
+    def _longest_encoded_length(self):
+        max_length = 0
+        for encoded_text in self.encoded_texts:
+            encoded_length = len(encoded_text)
+            if encoded_length > max_length:
+                max_length = encoded_length
+        return max_length
+    
+
 
 def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")
@@ -33,3 +76,53 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
     )
 
     return dataloader
+
+
+# split dataset for training
+def random_split(df, train_frac, validation_frac):
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int(len(df) * validation_frac)
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+    return train_df, validation_df, test_df
+
+
+def create_spam_dataloader_with_split(tokenizer, batch_size=4, num_workers=0):
+    
+    train_dataset = SpamDataset(
+        csv_file="data/train.csv",
+        max_length=None,
+        tokenizer=tokenizer)
+
+    val_dataset = SpamDataset(
+        csv_file="data/validation.csv",
+        max_length=train_dataset.max_length,
+        tokenizer=tokenizer)
+
+    test_dataset = SpamDataset(
+        csv_file="data/test.csv",
+        max_length=train_dataset.max_length,
+        tokenizer=tokenizer)
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True)
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        drop_last=False)
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        drop_last=False)
+    
+    return train_loader, val_loader, test_loader
--- myllm/util.py
+++ myllm/util.py
@@ -1,5 +1,6 @@
 import torch
 import myllm.gpt as gpt
+import numpy as np
 
 
 def gpt_config(name="gpt2-small"):
@@ -42,4 +43,200 @@ def text_to_token_ids(text, tokenizer):
 def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0)
     return tokenizer.decode(flat.tolist())
+
+
+##############
+# LOAD MODEL
+##############
+
+# to lead a model we need to replace starting weights of gptmodel class
+def _assign_shape(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f'Shape mismatch. left: {left.shape}, right {right.shape}')
+    return torch.nn.Parameter(torch.tensor(right))
+
+# from book transfer weights
+# given openai's convention this method is necessary to integrate
+def load_openai_weights_into_gpt(gpt, params):           #1
+    gpt.pos_emb.weight = _assign_shape(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = _assign_shape(gpt.tok_emb.weight, params['wte'])
+
+    for b in range(len(params["blocks"])):     #2
+        q_w, k_w, v_w = np.split(                            #3
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = _assign_shape(
+            gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = _assign_shape(
+            gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = _assign_shape(
+            gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = _assign_shape(
+            gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = _assign_shape(
+            gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = _assign_shape(
+            gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+        gpt.trf_blocks[b].att.out_proj.weight = _assign_shape(
+            gpt.trf_blocks[b].att.out_proj.weight, 
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = _assign_shape(
+            gpt.trf_blocks[b].att.out_proj.bias, 
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+        gpt.trf_blocks[b].ff.layers[0].weight = _assign_shape(
+            gpt.trf_blocks[b].ff.layers[0].weight, 
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = _assign_shape(
+            gpt.trf_blocks[b].ff.layers[0].bias, 
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = _assign_shape(
+            gpt.trf_blocks[b].ff.layers[2].weight, 
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = _assign_shape(
+            gpt.trf_blocks[b].ff.layers[2].bias, 
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].norm1.scale = _assign_shape(
+            gpt.trf_blocks[b].norm1.scale, 
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = _assign_shape(
+            gpt.trf_blocks[b].norm1.shift, 
+            params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = _assign_shape(
+            gpt.trf_blocks[b].norm2.scale, 
+            params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = _assign_shape(
+            gpt.trf_blocks[b].norm2.shift, 
+            params["blocks"][b]["ln_2"]["b"])
+
+    gpt.final_norm.scale = _assign_shape(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = _assign_shape(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = _assign_shape(gpt.out_head.weight, params["wte"])
         
+
+##############
+# TRAIN MODEL
+##############
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    #logits = model(input_batch)
+    #loss = torch.nn.functional.cross_entropy(
+    #    logits.flatten(0,1), target_batch.flatten()
+    #)
+    logits = model(input_batch)[:, -1, :]
+    loss = torch.nn.functional.cross_entropy(logits, target_batch)
+    
+    return loss
+
+# compute training and validation loss
+#1 Iteratives over all batches if no fixed num_batches is specified 
+#2 Reduces the number of batches to match the total number of batches in the data loader if num_batches exceeds the number of batches in the data loader 
+#3 Sums loss for each batch 
+#4 Averages the loss over all batches
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()
+        else:
+            break
+
+    return total_loss / num_batches
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval() # evaluation step not training
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded,
+            max_new_tokens=50, context_size=context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+
+def train_model_simple(model, train_loader, val_loader, 
+                        optimizer, device, num_epochs,
+                        eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [],[],[]
+    tokens_seen, global_step = 0,-1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                # train model
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (step {global_step:06d}): "
+                      f"Train loss {train_loss: .3f}"
+                      f"Val loss {val_loss: .3f}"
+                      )
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    
+    return train_losses, val_losses, track_tokens_seen
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
+def plot_learning_losses(num_epochs, tokens_seen, train_losses, val_losses):
+    epochs_seen = torch.linspace(0, num_epochs, len(train_losses))
+    fig, ax1 = plt.subplots(figsize=(5,3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()                   #1
+    ax2.plot(tokens_seen, train_losses, alpha=0)     #2
+    ax2.set_xlabel("Tokens seen")
+
+    fig.tight_layout()
+    plt.title('overfitting visualization')
+    plt.show()
