f8842298ea587726e9af433660eb027d35946bb9
Victor Hugo Germano <victorhg@gmail.com>
2025-04-29 15:02:52 -0700

    setup instructions dataset and loading model for pretraining

--- myllm/data.py
+++ myllm/data.py
@@ -3,6 +3,8 @@ import tiktoken
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
+PAD_TOKEN_ID = 50256  # <|endoftext|> token from gpt2
+
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
         self.input_ids = []
@@ -23,7 +25,7 @@ class GPTDatasetV1(Dataset):
 
 # Padding
 class SpamDataset(Dataset):
-    PAD_TOKEN_ID = 50256  # <|endoftext|> token from gpt2
+   
     def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=PAD_TOKEN_ID):
         self.data = pd.read_csv(csv_file)
         self.encoded_texts = [ tokenizer.encode(text) for text in self.data["Text"] ]
@@ -125,4 +127,131 @@ def create_spam_dataloader_with_split(tokenizer, batch_size=4, num_workers=0):
         num_workers=num_workers,
         drop_last=False)
     
+    return train_loader, val_loader, test_loader
+
+################
+## Instruction Training
+################
+from functools import partial
+import json
+
+def format_instruction_input(entry):
+    instruction_text = (
+        f"Bellow is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request." 
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
+
+    input_text = (
+        f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+    )
+
+    return instruction_text + input_text
+
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.encoded_texts = []
+
+        for entry in data:
+            instruction_plus_input = format_instruction_input(entry)
+            response_text = f"\n\n### Response:\n{entry['output']}"
+            full_text = instruction_plus_input + response_text
+            self.encoded_texts.append(
+                tokenizer.encode(full_text)
+            )
+
+    def __getitem__(self, index):
+        return self.encoded_texts[index]
+    
+    def __len__(self):
+        return len(self.data)
+
+
+def custom_instruction_collate_fn(batch, pad_token_id=PAD_TOKEN_ID, 
+                      ignore_index=-100, allowed_max_length=None, device="cpu"):
+    batch_max_length = max(len(item) + 1 for item in batch)
+
+    inputs_lst, target_lst = [], []
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets= targets[:allowed_max_length]
+
+
+        inputs_lst.append(inputs)
+        target_lst.append(targets)
+    
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    target_tensor = torch.stack(target_lst).to(device)
+    
+    return inputs_tensor, target_tensor
+
+def split_instruction_data(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    
+    # Set splits for training
+    train_portion = int(len(data) * 0.85)
+    test_portion = int(len(data) * 0.1)
+    val_portion = len(data) - train_portion - test_portion
+
+    train_data = data[:train_portion]
+    test_data = data[train_portion:train_portion + test_portion]
+    val_data = data[train_portion + test_portion:]
+    return train_data, val_data, test_data
+
+def create_instruction_data_loaders(train_data, val_data, test_data, 
+                                    batch_size, num_workers, tokenizer, device):
+
+    # We create a partial function to overrite the collate method within the DataLoader
+    customized_collate_fn = partial(
+        custom_instruction_collate_fn,
+        device=device,
+        allowed_max_length = 1024
+    )
+    train_dataset = InstructionDataset(train_data, tokenizer)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers
+    )
+
+    val_dataset = InstructionDataset(val_data, tokenizer)
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers 
+    )
+
+
+    test_dataset = InstructionDataset(test_data, tokenizer)
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers 
+    )
     return train_loader, val_loader, test_loader
